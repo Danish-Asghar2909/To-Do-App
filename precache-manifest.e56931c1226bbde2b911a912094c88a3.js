3ac886cfcc64e3fe454bc8f2f69fd8109731b697
Danish-Asghar2909 <danish.2909@icloud.com>
2020-08-22 06:42:51 -0700

    Updates

---
+++ precache-manifest.e56931c1226bbde2b911a912094c88a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f912556d91463e101625601cb38ec7e",
+    "url": "/To-Do-App/index.html"
+  },
+  {
+    "revision": "3b888a32cb513303e670",
+    "url": "/To-Do-App/static/css/main.aa0ca250.chunk.css"
+  },
+  {
+    "revision": "864bb3a41ce8adfdd052",
+    "url": "/To-Do-App/static/js/2.63ba9af2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-Do-App/static/js/2.63ba9af2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b888a32cb513303e670",
+    "url": "/To-Do-App/static/js/main.bcb8a34d.chunk.js"
+  },
+  {
+    "revision": "4de508d07532b2ea1c01",
+    "url": "/To-Do-App/static/js/runtime-main.1e693c49.js"
+  }
+]);
